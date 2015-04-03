c5abfe202ed01f4525be30efcdf1ed59e82cf007
Ashutosh Mishra <ashutosh.mishra@labs108.com>
2015-04-03 11:55:36 +0530

    Initial project added

---
+++ BMSLocationFinder/BMSLocationFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMSLocationFinder
+//
+//  Created by Ashutosh on 03/04/2015.
+//  Copyright (c) 2015 Ashutosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
